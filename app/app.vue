b26c6fffa8418da13c6001171a581c67f18f033a
Давид А. Саркисян <sarkisyanda@rubezh.local>
2026-02-20 17:04:00 +0400

    init

---
+++ app/app.vue
@@ -0,0 +1,7 @@
+<template>
+  <MainPageTemplate />
+</template>
+
+<script setup>
+import MainPageTemplate from '../components/templates/MainPageTemplate.vue'
+</script>
---
+++ components/templates/MainPageTemplate.vue
@@ -0,0 +1,23 @@
+<template>
+  <Header />
+  <!-- <HeroSection />
+  <MySkills />
+  <MyExperience />
+  <AboutMe />
+  <MyProjects />
+  <MyTestimonial />
+  <ContactSection />
+  <Footer /> -->
+</template>
+
+<script setup>
+import Header from '../organisms/Header.vue'
+import HeroSection from '../organisms/HeroSection.vue'
+import MySkills from '../organisms/MySkills.vue'
+import MyExperience from '../organisms/MyExperience.vue'
+import AboutMe from '../organisms/AboutMe.vue'
+import MyProjects from '../organisms/MyProjects.vue'
+import MyTestimonial from '../organisms/MyTestimonial.vue'
+import ContactSection from '../organisms/ContactSection.vue'
+import Footer from '../organisms/Footer.vue'
+</script>
